21c9754a0e604c64de5eb089348d9d00cbe85b74
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-27 20:16:39 +0530

    refactor proxy.router.js and add more routes

--- api-gateway/src/routes/proxy.routes.js
+++ api-gateway/src/routes/proxy.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const role = require("../middlewares/role.middleware");
 
+// 🔓 AUTH (PUBLIC)
 router.use(
   "/auth",
   createProxyMiddleware({
@@ -11,34 +13,62 @@ router.use(
   })
 );
 
+// 🧑 PATIENT SERVICE
 router.use(
   "/patients",
+  role("PATIENT", "DOCTOR", "ADMIN"),
   createProxyMiddleware({
     target: process.env.PATIENT_SERVICE_URL,
     changeOrigin: true,
   })
 );
 
-router.get(
+// 👨‍⚕️ DOCTOR SERVICE
+router.use(
   "/doctors",
+  role("DOCTOR", "ADMIN"),
   createProxyMiddleware({
     target: process.env.DOCTOR_SERVICE_URL,
     changeOrigin: true,
   })
 );
 
+// 📅 APPOINTMENTS
 router.use(
   "/appointments",
+  role("PATIENT", "DOCTOR", "STAFF", "ADMIN"),
   createProxyMiddleware({
     target: process.env.APPOINTMENT_SERVICE_URL,
     changeOrigin: true,
   })
 );
 
+// 💳 BILLING
 router.use(
   "/billing",
+  role("STAFF", "ADMIN"),
   createProxyMiddleware({
     target: process.env.BILLING_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+// 🧾 STAFF
+router.use(
+  "/staff",
+  role("STAFF", "ADMIN"),
+  createProxyMiddleware({
+    target: process.env.STAFF_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+// 👑 ADMIN
+router.use(
+  "/admin",
+  role("ADMIN"),
+  createProxyMiddleware({
+    target: process.env.ADMIN_SERVICE_URL,
     changeOrigin: true,
   })
 );
